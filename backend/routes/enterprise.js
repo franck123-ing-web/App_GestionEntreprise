fa75c73f9b01ffcc5dd15ebf21f7d5046ca58f8f
franck joel <gamaspeed380@gmail.com>
2025-12-04 17:44:43 +0100

    Update enterprise.js

--- backend/routes/enterprise.js
+++ backend/routes/enterprise.js
@@ -7,5 +7,6 @@ router.get("/:id", enterpriseController.getOne);
 router.post("/", enterpriseController.create);
 router.put("/:id", enterpriseController.update);
 router.delete("/:id", enterpriseController.deleteOne);
+router.get("/search/:name", enterpriseController.searchByName);
 
 module.exports = router;
